79f653ee47dae668008fdd0d55d2d7e6500b2c0e
Pinkolik <mrpinkolik@gmail.com>
2022-09-01 18:26:51 +0500

    initial commit

---
+++ src/main/java/io/github/pinkolik/episodeswitcher/EpisodeSwitcherApplication.java
@@ -0,0 +1,13 @@
+package io.github.pinkolik.episodeswitcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpisodeSwitcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpisodeSwitcherApplication.class, args);
+    }
+
+}
