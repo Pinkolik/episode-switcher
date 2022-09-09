4c853e1c1982dd3ba3b8a42dccb1d12259d6ca34
Pinkolik <mrpinkolik@gmail.com>
2022-09-09 14:04:23 +0500

    fix deserialization

--- src/main/java/io/github/pinkolik/episodeswitcher/alisa/dto/common/enums/CapabilityType.java
+++ src/main/java/io/github/pinkolik/episodeswitcher/alisa/dto/common/enums/CapabilityType.java
@@ -1,31 +1,17 @@
 package io.github.pinkolik.episodeswitcher.alisa.dto.common.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RequiredArgsConstructor
 public enum CapabilityType {
-    ON_OFF("devices.capabilities.on_off"),
-    RANGE("devices.capabilities.range");
-
-    private static final Map<String, CapabilityType> NAMES_MAP = new HashMap<>();
+    @JsonProperty("devices.capabilities.on_off") ON_OFF("devices.capabilities.on_off"),
+    @JsonProperty("devices.capabilities.range") RANGE("devices.capabilities.range");
 
     @Getter
     @JsonValue
     private final String name;
 
-    static {
-        NAMES_MAP.put("devices.capabilities.on_off", ON_OFF);
-        NAMES_MAP.put("devices.capabilities.range", RANGE);
-    }
-
-    @JsonCreator
-    public static CapabilityType forName(String name) {
-        return NAMES_MAP.get(name);
-    }
 }
--- src/main/java/io/github/pinkolik/episodeswitcher/alisa/dto/common/enums/InstanceType.java
+++ src/main/java/io/github/pinkolik/episodeswitcher/alisa/dto/common/enums/InstanceType.java
@@ -1,19 +1,14 @@
 package io.github.pinkolik.episodeswitcher.alisa.dto.common.enums;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonValue;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.HashMap;
-import java.util.Map;
-
 @RequiredArgsConstructor
 public enum InstanceType {
-    CHANNEL("channel", null),
-    VOLUME("volume", Constants.PERCENT);
-
-    private static final Map<String, InstanceType> NAMES_MAP = new HashMap<>();
+    @JsonProperty("channel") CHANNEL("channel", null),
+    @JsonProperty("volume") VOLUME("volume", Constants.PERCENT);
 
     @Getter
     @JsonValue
@@ -22,15 +17,6 @@ public enum InstanceType {
     @Getter
     private final String unit;
 
-    static {
-        NAMES_MAP.put("channel", CHANNEL);
-        NAMES_MAP.put("volume", VOLUME);
-    }
-
-    @JsonCreator
-    public static InstanceType forName(String name) {
-        return NAMES_MAP.get(name);
-    }
 
     private static class Constants {
 
